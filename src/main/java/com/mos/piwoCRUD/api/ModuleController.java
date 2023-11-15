31d134fbace8bf1546a28afd40c44749ca16ecf6
Jakub Żak <jakub.jacqes@gmail.com>
2023-11-15 13:26:37 +0100

    adding some code to module controller class

--- src/main/java/com/mos/piwoCRUD/api/ModuleController.java
+++ src/main/java/com/mos/piwoCRUD/api/ModuleController.java
@@ -1,4 +1,18 @@
 package com.mos.piwoCRUD.api;
 
+import com.mos.piwoCRUD.service.ModuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api")
+@RestController
 public class ModuleController {
+    private final ModuleService moduleService;
+@Autowired
+    public ModuleController(ModuleService moduleService) {
+        this.moduleService = moduleService;
+    }
+    
+
 }
